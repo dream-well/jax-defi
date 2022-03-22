d933e0f42992ce1e2dea95a739b2d576a365aff1
dream-well <dreamwell506@outlook.com>
2022-03-22 17:07:02 +0800

    delete: verify.sh

--- verify.sh
+++
@@ -1,8 +0,0 @@
-npx hardhat verify 0x2062736C84FC2a4554c82F3Af80e8b91e8dfA71C "Pegged USD Binance" "BUSD" 18
-npx hardhat verify 0xD576C53A43Aed9EDF97e134DcE7B9Cdc328B7429 "Wrapped Jaxnet" "WJXN" 0
-npx hardhat verify 0xCeAb7e9BF15E6Ca847C17E6bd35be30a033c916D "Wrapped Jax" "WJAX" 4
-npx hardhat verify 0x39018b657FAfE5781712b7333B68952116E8e334 "Jax Dollar" "JAX DOLLAR" 18 --contract contracts/JaxToken.sol:JaxToken
-npx hardhat verify 0x93C3D27fC3Fce087b230FFd1acfD717B43Dc2c86 "Jax Rupee" "JAX RUPEE" 18 --contract contracts/JaxToken.sol:JaxToken
-  
-  
-  
